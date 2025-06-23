bf56a417f64358f68f78d8145dd9eeea64257d1e
augistlk <augistlk@gmail.com>
2025-06-23 19:46:00 +0300

    Actually works

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import subprocess
 import random
-#variable "test" is path to the .cpp file
-def compileC(test: str):
-    compile_result = subprocess.run(["g++", test, "-o", "hello"], capture_output=True, text=True)
+import os
+#variable fileLocation is path to the .cpp file
+def compileC(fileLocation: str, test: int, testType:str):
+    compile_result = subprocess.run(["g++", fileLocation, "-o", "test/a.exe"], capture_output=True, text=True)
 
     if compile_result.returncode != 0:
-        print(f"Compilation of {test} failed:")
+        print(f"Compilation of {testType} #{test} failed:")
         print(compile_result.stderr)
     else:
-        print(f"Compilation {test} successful!")
+        print(f"Compilation {testType} #{test} successful!")
 
 def inputGenerator():
     f = open("test/args.txt")
-    amountOfTests: int = int(f.readline())
+    global amountOfTests
+    amountOfTests = int(f.readline())
     amountOfVariables: int = int(f.readline())
     variables = []
     #variables[x][y]  x is variable number, y is range(1 is from, 2 is to)
@@ -32,4 +34,35 @@ def inputGenerator():
         output.write("\n")
     output.close()
 
-inputGenerator()
+#testType must be either "Test" or "User"
+def compileAndRun(fileLocation: str, testNumber: int, testType: str):
+    compileC(fileLocation, testNumber, testType)
+    executableLocation = fileLocation.split("/")[0] + "/a.exe"
+    runResult = subprocess.run([executableLocation])
+    if runResult.returncode !=0:
+        print(f"{testType} #{testNumber} didn't return 0")
+    if testType == "Test":
+            os.replace("result.txt", "test/testResult.txt")
+    else:
+        os.replace("result.txt", "test/result.txt")
+        
+
+def runAndCompareResults():
+    global amountOfTests
+    for i in range(0, amountOfTests):
+        compileAndRun("test/test.cpp", i+1, "Test")
+        compileAndRun("test/user.cpp", i+1, "User")
+        testResult = open("test/testResult.txt", "r")
+        userResult = open("test/result.txt", "r")
+        if testResult.read() == userResult.read():
+            print(f"Test #{i+1} passed")
+        else:
+            print(f"Test #{i+1} failed")
+        testResult.close()
+        userResult.close()
+
+def main():
+    inputGenerator()
+    runAndCompareResults()
+
+main()
