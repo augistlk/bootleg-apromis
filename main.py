62c1f4b8888efb9d5f5fe8a64a8bfa3685f83f39
Augustas Tolkačiovas <augistlk@gmail.com>
2025-07-18 19:15:17 +0300

    Improved programming of the random test generator, now supports different variable types and file headers. Added a cleanup function. Converted it to run on linux, because I main it now.
    Signed-off-by: Augustas Tolkačiovas <augistlk@gmail.com>

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import string
 #variable fileName is the name of the .cpp file in the test/ directory
 def compileC(fileName: str, testType:str):
-    compile_result = subprocess.run(["g++", fileName, "-o", f"{testType}.exe"], capture_output=True, text=True, cwd="test")
+    compile_result = subprocess.run(["g++", fileName, "-o", f"{testType}"], capture_output=True, text=True, cwd="test")
 
     if compile_result.returncode != 0:
         print(f"Compilation of {testType} failed:")
@@ -18,29 +18,74 @@ def compileC(fileName: str, testType:str):
 
 def inputGenerator():
     f = open("test/args.txt")
-    global amountOfTests
-    amountOfTests = int(f.readline())
-    amountOfTestsForFile = int(f.readline())
-    amountOfVariables: int = int(f.readline())
+
+    f.readline() #to skip the test line, cause it's already been read
+
+    global line
+    global keyword
+    global value
+    global secondValue
+
+    def readLine():
+        global line
+        global keyword
+        global value
+        global secondValue
+        line = f.readline()
+        keyword = line.split(" ")[0]
+        value = int(line.split(" ")[1].rstrip(":\n"))
+        if keyword == "int" or keyword == "timesrepeated":
+            if keyword == "timesrepeated":
+                try: secondValue = int(line.split(" ")[2].rstrip(":\n"))
+                except: secondValue = None
+            else:
+                secondValue = int(line.split(" ")[2].rstrip(":\n"))
+
+    
+    header = ""
+    readLine()
+    while keyword != "variables":
+        if keyword == "headervariables":
+            amountOfHeaderVariables = int(value)
+            for i in range(0,amountOfHeaderVariables):
+                readLine()
+                match keyword:
+                    case "timesrepeated":
+                        if secondValue != None:
+                            randomNumber = random.randint(value, secondValue)
+                            header += str(randomNumber)
+                            amountOfTestsForFile = randomNumber
+                        else:
+                            header += str(value)
+                            amountOfTestsForFile = value
+                        if i != amountOfHeaderVariables:
+                            header += " "
+                    case "int":
+                        header += str(random.randint(value, secondValue)) #value is smallest value, secondValue is largest value
+                    case default:
+                        print(f"Unexpected header type '{keyword}'. Terminating process.")
+                        exit()
+        readLine()
+    amountOfVariables = value #because the last line read will have been variables
     variables = []
-    allowedTypes = ['int', 'string']
+    allowedVariableTypes = ['int', 'string']
     #variables[i][x] i is index, x when 0 is type, if type is string 1 is length, else 1 is range from, 2 is range to
     for i in range(0,amountOfVariables):
         line = f.readline()
         type = line.split(" ")[0]
-        if type not in allowedTypes:
+        if type not in allowedVariableTypes:
             print(f"Unexpected type '{type}'. Only supported types are so far 'int' and 'string'\n")
             print("Terminating process")
             exit()
-        start = int(line.split(" ")[1])
+        one = int(line.split(" ")[1])
         if type != "string":
-            end = int(line.split(" ")[2])
+            two = int(line.split(" ")[2])
         else:
-            end = 0
-        variables.append((type, start, end))
+            two = 0
+        variables.append((type, one, two))
     f.close()
     output = open("test/data.txt", "w")
-    output.write(f"{amountOfTestsForFile}\n")
+    output.write(f"{header}\n")
     for i in range(0,amountOfTestsForFile):
         for j in range(0, amountOfVariables):
             if variables[j][0] == "string":
@@ -59,12 +104,13 @@ def inputGenerator():
 def initializer():
     f = open("test/args.txt")
     global amountOfTests
-    amountOfTests = int(f.readline())
+    line = f.readline()
+    amountOfTests = int(line.split(" ")[1])
     f.close()
 
 #testType must be either "test" or "user"
 def runTest(testNumber: int, testType: str):
-    executableFilename = f"test/{testType}.exe"
+    executableFilename = f"./{testType}"
     runResult = subprocess.run([executableFilename], cwd="test")
     if runResult.returncode !=0:
         print(f"{testType} #{testNumber} didn't return 0")
@@ -95,8 +141,17 @@ def runAndCompareResults():
         userResult.close()
     testLog.close()
 
+def cleanUp():
+    os.remove("test/result.txt")
+    os.remove("test/data.txt")
+    os.remove("test/test")
+    os.remove("test/user")
+    os.remove("test/testResult.txt")
+    print("Cleanup successful!")
+
 def main():
     initializer()
     runAndCompareResults()
+    cleanUp()
 
 main()
